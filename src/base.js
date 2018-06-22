95d3138e3785846523a1364d61923db047e7e28e
ksunilssah <ksunilssah@gmail.com>
2018-06-22 09:18:14 +0530

    git email id updated

--- src/base.js
+++ src/base.js
@@ -2,9 +2,9 @@ import Rebase from 're-base';
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyDqkcqdeiR2p5xTyli536hZ070Q_HSuTMg",
+  /*  apiKey: "AIzaSyDqkcqdeiR2p5xTyli536hZ070Q_HSuTMg",
     authDomain: "shopping-cart-react.firebaseapp.com",
-    databaseURL: "https://shopping-cart-react.firebaseio.com",
+    databaseURL: "https://shopping-cart-react.firebaseio.com",*/
     /*   projectId: "shopping-cart-react",
        storageBucket: "shopping-cart-react.appspot.com",
        messagingSenderId: "191378510850"*/
